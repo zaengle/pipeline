a720020cf6e42fe515106f12e092e4105f49231b
Jesse Schutt <jesse@zaengle.com>
2020-04-30 16:56:35 -0500

    Adds pipe interface

---
+++ src/Contracts/PipeInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Zaengle\Pipeline\Contracts;
+
+/**
+ * Interface PipeInterface
+ */
+interface PipeInterface {
+  /**
+   * @param $traveler
+   * @param \Closure $next
+   * @return mixed
+   */
+  public function handle($traveler, \Closure $next);
+}
